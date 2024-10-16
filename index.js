32555aefc6858caf65127f8ce0af6e256d02fd18
Bautin Stepan bc2024-4 <stepanbaytun@gmail.com>
2024-10-17 00:00:34 +0300

    the GET method was added

--- index.js
+++ index.js
@@ -12,20 +12,37 @@ const { host, port, cache } = program.opts();
 
 const server = http.createServer(async function (req, res) {
     console.log(`Received request: ${req.method} ${req.url}`);
+    const statusCode = req.url.slice(1);
+    const filePath = `${cache}/${statusCode}.jpg`;
     if (req.method === 'PUT') {
-        const statusCode = req.url.slice(1);
         console.log(`Processing PUT request for status code: ${statusCode}`);
 
         let body = [];
         req.on('data', chunk => body.push(chunk));
         req.on('end', async () => {
             body = Buffer.concat(body);
-            const filePath = `${cache}/${statusCode}.jpg`;
             console.log(`Saving image to: ${filePath}`);
             await fs.writeFile(filePath, body);
             res.writeHead(201, {'Content-Type': 'text/plain'});
             res.end('Image saved');
         });
+    } else if (req.method === 'GET') {
+        console.log(`Processing GET request for status code: ${statusCode}`);
+
+        try {
+            const image = await fs.readFile(filePath);
+            res.writeHead(200, {'Content-Type': 'image/jpeg'});
+            res.end(image);
+        } catch (err) {
+            try {
+                const image404 = await fs.readFile(`${cache}/404.jpg`);
+                res.writeHead(404, {'Content-Type': 'image/jpeg'});
+                res.end(image404);
+            } catch (err) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('404 image not found');
+            }
+        }
     } else {
         res.writeHead(405, {'Content-Type': 'text/plain'});
         res.end('Method not allowed');
