353e66cd13df1d6c8894f3685103e4bb1f9b93b0
Bautin Stepan bc2024-4 <stepanbaytun@gmail.com>
2024-10-16 23:45:12 +0300

    the PUT method was added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require("node:fs");
+const fs = require("fs").promises;
 const { program } = require('commander');
 
 program
@@ -8,15 +8,30 @@ program
     .requiredOption('-c, --cache <path>', 'path to the cache directory');
 
 program.parse();
-
 const { host, port, cache } = program.opts();
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    res.end();
+const server = http.createServer(async function (req, res) {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    if (req.method === 'PUT') {
+        const statusCode = req.url.slice(1);
+        console.log(`Processing PUT request for status code: ${statusCode}`);
+
+        let body = [];
+        req.on('data', chunk => body.push(chunk));
+        req.on('end', async () => {
+            body = Buffer.concat(body);
+            const filePath = `${cache}/${statusCode}.jpg`;
+            console.log(`Saving image to: ${filePath}`);
+            await fs.writeFile(filePath, body);
+            res.writeHead(201, {'Content-Type': 'text/plain'});
+            res.end('Image saved');
+        });
+    } else {
+        res.writeHead(405, {'Content-Type': 'text/plain'});
+        res.end('Method not allowed');
+    }
 });
 
-server.listen(port, host, () =>{
+server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
